7b5f494a484be14a452a7b3695462becddcee6f5
Lud Oliva <olivalud12@gmail.com>
2025-10-17 17:02:31 -0300

    faxina

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // ATENÇÃO LUUUD ---> Adicionar o caminho para todos os seus arquivos que usam classes NativeWind
   content: [
-    
+    "./App.{js,jsx,ts,tsx}", 
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
