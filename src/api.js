f943b5fc21edaff248091fde0fa766cd1ab226e5
= <=>
2025-10-21 22:00:30 -0300

    Pegar automaticamente o ip do backend

---
+++ src/api.js
@@ -0,0 +1,12 @@
+import { NativeModules } from "react-native";
+
+const getHostIp = () => {
+    const scriptURL = NativeModules?.SourceCode?.scriptURL
+    if (scriptURL) {
+        const host = scriptURL.split("://")[1].split(":")[0];
+        return host;
+    }
+    return "192.168.0.21"
+};
+
+export const API_URL = `http://${getHostIp()}:8080`;
