ecec5d18005df596f842f337f5279aa97841aaf5
Lud Oliva <olivalud12@gmail.com>
2025-11-07 19:48:08 -0300

    correção do ''administrado'', para melhor coesão

--- app/home.tsx
+++ app/home.tsx
@@ -419,7 +419,7 @@ export default function HomeScreen() {
                   color="#FFF"
                 />
                 <Text style={styles.statusButtonText}>
-                  {med.status === 'concluido' ? 'Concluído' : 'Marcar como Administrado'}
+                  {med.status === 'concluido' ? 'Concluído' : 'Marcar como Concluído'}
                 </Text>
               </Pressable>
 
